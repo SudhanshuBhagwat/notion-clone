c6eec7e6dc6f8249d8ed62d41ea4feebd5e5eb7b
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-08-25 20:54:59 +0530

    Added change to focus last element in the blocks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Block, { BlockType } from "../components/Block";
 import { nanoid } from 'nanoid'
 import { proxy, useSnapshot } from "valtio";
+import { useEffect, useRef } from "react";
 
 interface IBlock {
   id: string;
@@ -32,16 +33,26 @@ export function addBlock() {
 }
 
 export function removeBlock() {
-  state.blocks.pop();
+  if (state.blocks.length > 1) {
+    state.blocks.pop();
+  }
 }
 
 const Home: NextPage = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
   const { blocks } = useSnapshot(state); 
+
+  useEffect(() => {
+    const allBlocks = containerRef.current?.querySelectorAll("div");    
+    if (allBlocks) {
+      allBlocks[allBlocks?.length - 1]?.focus();
+    }
+  }, [blocks]);
 
   return (
     <div>
       <h1 className="text-4xl font-bold">Home</h1>
-      <div className="mt-4 flex flex-col gap-2">
+      <div ref={containerRef} className="mt-4 flex flex-col gap-2">
         {blocks.map(block => (
           <Block key={block.id} type={block.type}/>
         ))}
